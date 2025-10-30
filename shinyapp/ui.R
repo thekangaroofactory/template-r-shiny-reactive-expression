85d7105aefd4d085e24b8adad127d29819241b51
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-10-30 16:53:32 +0100

    First commit

---
+++ shinyapp/ui.R
@@ -0,0 +1,32 @@
+
+
+# ------------------------------------------------------------------------------
+# User-interface definition of the Shiny application
+# ------------------------------------------------------------------------------
+
+# -- Define & return UI
+page_navbar(
+
+  # -- Footer
+  fillable = FALSE,
+  footer = p(style = "font-size:9pt;margin-top:20px;", "© 2025 Philippe Peret"),
+
+  # -- Theme
+  theme = bs_theme(
+    bg = "#2d3037",
+    fg = "#FFF",
+    primary = "orange",
+    secondary = "#ececec",
+    base_font = font_google("Quicksand")),
+
+  # -- Title
+  title = "App Title",
+
+  # -- First tab
+  nav_panel(title = "Nav_1",
+            class = "p-5",
+
+            # -- content
+            p("Something goes here"))
+
+)
